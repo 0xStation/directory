78a1b2c954eaaf6379c62349a2936dbae9974448
Conner Swenberg <cls364@cornell.edu>
2024-05-20 22:36:54 -0700

    Fix build

--- packages/next/groups/index.ts
+++ packages/next/groups/index.ts
@@ -1,13 +1,7 @@
-import arcade from "./arcade.config";
 import bytexplorers from "./bytexplorers.config";
-import gaspass from "./gaspass.config";
-import mangrove from "./mangrove.config";
 import noms from "./noms.config";
 
 export default {
-  arcade,
   bytexplorers,
-  gaspass,
-  mangrove,
   noms,
 };
